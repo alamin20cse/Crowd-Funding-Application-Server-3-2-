f261c3487356e006c96df588d4b0ec3e9c551c8d
alamin20cse <md.alamin165086@gmail.com>
2025-02-22 22:43:52 +0600

    campaig  added and all campaing get

---
+++ index.js
@@ -0,0 +1,109 @@
+const express=require('express');
+const cors=require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app=express();
+const port =process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uslpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection=client.db('CrowdFundingDB').collection('users');
+    const campignCollection=client.db('CrowdFundingDB').collection('campain');
+    
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    
+    app.post('/users',async(req,res)=>{
+      const newUser=req.body;
+      // console.log(newUser);
+      const result=await userCollection.insertOne(newUser);
+      res.send(result);
+  
+  })
+
+  
+  app.post('/campign',async(req,res)=>{
+    const newCampign=req.body;
+    console.log(newCampign);
+    const result=await campignCollection.insertOne(newCampign);
+    res.send(result);
+   
+})
+// get all campains
+app.get('/allcampign',async(req,res)=>{
+    const cursor=campignCollection.find();
+    const result=await cursor.toArray();
+    res.send(result);
+
+
+})
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('crowd funding running   .. ');
+});
+
+app.listen(port,()=>{
+
+    console.log(`crowd funding is running on port :${port}`);
+
+})
